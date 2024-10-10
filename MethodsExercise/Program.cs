106b2f32febcb126b3f2406daaa4f5bcba04a1c0
Kourtnee Harris <jhtech1111@gmail.com>
2024-10-10 16:28:58 -0400

    Created a C# Story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+        }
+        public static void MadLib()
+        {
+           Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello,{userName}! What is your favorite animal");
+            string favAnimal  = Console.ReadLine();
+            Console.WriteLine(" What is your favorite color");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite band"); 
+                string favBand = Console.ReadLine();
+
+            Console.WriteLine($"On {userName} birthday, she decided to wear a {favColor} dress to see {favBand}. After the concert {userName} boyfriend gifted her a {favAnimal}. Best birthday ever!");
         }
     }
+
 }
