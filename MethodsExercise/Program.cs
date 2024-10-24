47461911c262d8d1ee1e4de0bc567d7bd5527e5d
Kourtnee Harris <jhtech1111@gmail.com>
2024-10-24 14:39:56 -0400

    Completed Exercise 2 with math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,10 +2,7 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            MadLib();
-        }
+       
         public static void MadLib()
         {
            Console.WriteLine("What is your name?");
@@ -19,6 +16,42 @@
 
             Console.WriteLine($"On {userName} birthday, she decided to wear a {favColor} dress to see {favBand}. After the concert {userName} boyfriend gifted her a {favAnimal}. Best birthday ever!");
         }
+        static void Main(string[] args)
+        {
+            //MadLib();
+            //Console.WriteLine(Add(4, 4));
+            // Console.WriteLine(Subtract(4, 4));
+            //Console.WriteLine(Multiply(3, 3));
+            //Console.WriteLine(Divide(3, 3));
+            Console.WriteLine(Sum(6, 6, 6));
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+        public static int Subtract( int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return  numOne * numTwo;
+        }
+        public static int Divide(int numOne, int numTwo)
+        { 
+            return (numOne / numTwo);
+        }
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
     }
 
 }
+
